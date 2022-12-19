19faf4b8b739c590675cf37fcc521e26818548dd
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2022-12-19 17:56:57 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hellow world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`server is running on ${port}`));
