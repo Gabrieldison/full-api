3871c8b32470f0a6b585e411f5ae4004abfd918e
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2023-01-01 19:31:41 -0300

    feat: new features

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import { MongoClient } from "./database/mongo";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { GetUsersController } from "./controllers/get-users/get-users";
 
 const main = async () => {
   config();
@@ -8,6 +10,16 @@ const main = async () => {
   const app = express();
 
   await MongoClient.connect();
+
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+    const { body, statusCode } = await getUsersController.handle();
+
+    res.send(body).status(statusCode);
+  });
 
   const port = process.env.PORT || 8000;
 
